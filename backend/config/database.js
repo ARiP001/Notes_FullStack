4c66a1dbb773c4c8d1a15dec9d0e0bf9e2589edd
ARiP001 <randomguydx@gmail.com>
2025-03-04 13:42:04 +0700

    ganti gcp

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('notes','root','', {
-    host:'localhost',
+const db = new Sequelize('notes_arif','root','', {
+    host:'34.46.79.48',
     dialect:'mysql'
 })
 
