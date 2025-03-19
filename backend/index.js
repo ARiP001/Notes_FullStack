666acbc72fa11f3e691149219516041fe788c586
ARiP001 <randomguydx@gmail.com>
2025-03-19 15:15:32 +0700

    ejs

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,11 @@ import cors from "cors";
 import NoteRoutes from "./routes/NoteRoutes.js";
 
 const app = express();
+app.set("view engine", "ejs");
+
 app.use(cors());
 app.use(express.json());
+app.get("/", (req, res) => res.render("index"));
 app.use(NoteRoutes);
 
 app.listen(7000, () => {
